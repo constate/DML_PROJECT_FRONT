05190771e7e1e3045722a3fa8c988fae1b9329c4
haerinee <haerinelee@gmail.com>
2025-05-13 08:38:00 +0900

    fix : package.json 파일 업로드, selectPage 처음으로 버튼 추가

--- src/pages/Select/SelectPage.jsx
+++ src/pages/Select/SelectPage.jsx
@@ -6,16 +6,32 @@ export const SelectPage = () => {
   const navigate = useNavigate();
   const setOrderType = useKioskStore((state) => state.setOrderType);
 
+  // 처음으로
+  const reset = useKioskStore((state) => state.reset);
+
   const handleSelect = (type) => {
     setOrderType(type);
     navigate('/menu');
   };
 
+  const handleReset = () => {
+    reset(); // 상태 초기화
+    navigate('/kiosk'); // 시작하기로 이동
+  };
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-white px-4">
+    <div className="relative flex h-screen flex-col items-center justify-center bg-white px-4">
+      {/* 상단 처음으로 버튼 */}
+      <button
+        onClick={handleReset}
+        className="absolute top-4 left-4 rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow hover:bg-gray-100"
+      >
+        처음으로
+      </button>
+
       <h1 className="mb-12 text-center text-3xl font-bold text-gray-800">
         이용 방식을 선택해주세요
       </h1>
+
       <div className="flex w-full max-w-md flex-col gap-6 md:flex-row">
         <button
           onClick={() => handleSelect('eat-in')}
